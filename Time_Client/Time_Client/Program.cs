b511bc7f635d8da091e9d67e3dccafad0ca6bfcf
Bek <begmurod1999@mail.ru>
2019-11-17 18:23:17 +0500

    Разработка клиент-серверного приложения

---
+++ Time_Client/Time_Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static int n; 
+        private static Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Номер клиетна:");
+            n = int.Parse(Console.ReadLine());
+            Console.Title = "Клиент:"+n;
+            LoopConnect();
+            SendLoop();
+            Console.ReadKey();
+        }
+
+        private static void SendLoop()
+        {
+            Random rand = new Random();
+            while (true)
+            {
+                Thread.Sleep(2700);
+                try
+                {
+                    double d = rand.Next(20, 30) * 0.99;
+                    byte[] buffer = Encoding.UTF8.GetBytes(d+" Клиент:"+n);
+                    clientSocket.Send(buffer);
+
+                    byte[] receivedBuf = new byte[1234];
+                    int rec = clientSocket.Receive(receivedBuf);
+                    byte[] data = new byte[rec];
+                    Array.Copy(receivedBuf, data, rec);
+                    Message("Получено: " + Encoding.UTF8.GetString(data));
+                }
+                catch (SocketException)
+                {
+                    Message("Сервер отключился");
+                    break;
+                }
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+            while (!clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    clientSocket.Connect(IPAddress.Loopback, 8888);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Message("Попытка соединения: " + attempts.ToString());
+                }
+            }
+            Console.Clear();
+            Message("Подключено");
+        }
+        
+        private static void Message(string str)
+        {
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff ") + str);
+        }
+    }
+}
---
+++ Time_Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private static byte[] buffer = new byte[1234]; 
+        private static List<Socket> clietnSockets = new List<Socket>(); 
+        private static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Сервер";
+            SetupServer();
+            Console.ReadKey();
+        }
+        private static void SetupServer() 
+        {
+            Message("Настройка сервера");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any,8888)); 
+            serverSocket.Listen(4); 
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback),null);
+        }
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            Socket socket = serverSocket.EndAccept(ar);
+            clietnSockets.Add(socket);
+            Message("Клиент подключен");
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            try
+            {
+                int received = socket.EndReceive(ar);
+                byte[] dataBuf = new byte[received];
+                Array.Copy(buffer, dataBuf, received);
+                string text = Encoding.UTF8.GetString(dataBuf);
+                string accepted = "";
+
+                Message("Сообщение получено: " + text);
+
+                Thread.Sleep(5000);
+                for (int i = 0; i<text.Length; i++)
+                {
+                    if (text[i] == 'К')
+                    {
+                        accepted = text.Remove(i - 1);
+                        break;
+                    }
+                }
+                string toSend = "Округленное значение "+Math.Round(Convert.ToDouble(accepted));
+
+                byte[] data = Encoding.UTF8.GetBytes(toSend); 
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket); 
+            }
+            catch (SocketException)
+            {
+                Message("Клиент отключился");
+            }
+            
+        }
+        private static void SendCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndSend(ar);
+        }
+        private static void Message(string str)
+        {
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff ") + str);
+        }
+    }
+}
